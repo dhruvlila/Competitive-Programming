3f647fe4c3011c6c32df03c0a0cb9feb74bbf925
Dhruv Lila <117848612+dhruvlila@users.noreply.github.com>
2024-06-07 05:23:27 +0530

    Create Repetitions.cpp

---
+++ CSES/IntroductoryProblems/Repetitions.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair <ll, ll> pll;
+typedef vector <ll> vll;
+ 
+#define push push_back
+#define pop pop_back
+
+const ll MOD = 1e9 + 7;
+
+void solve();
+
+int main(){
+
+   #ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+   #endif
+
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   solve();
+   return 0;
+
+}
+
+void solve(){
+
+   string s;
+   cin >> s;
+
+   ll n = s.length();
+   ll cnt = 1;
+   ll m = 1;
+   for(ll i = 1; i < n; i++){
+      if(s[i] == s[i-1]){
+         cnt++;
+      }
+      else{
+         cnt = 1;
+      }
+      m = max(m, cnt);
+   }
+
+   cout << m << endl;
+
+}
