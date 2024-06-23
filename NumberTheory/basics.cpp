458f613744f718ad19233dd5e1927039430059ba
Dhruv Lila <117848612+dhruvlila@users.noreply.github.com>
2024-06-23 18:24:16 +0530

    Update basics.cpp

--- NumberTheory/basics.cpp
+++ NumberTheory/basics.cpp
@@ -1,1 +1,95 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair <ll, ll> pll;
+typedef vector <ll> vll;
+
+const ll MOD = 1e9 + 7;
+
+class Graphs{
+public:
+
+   // Sieve of Eratosthenes to compute all primes from 1 --> n.
+   // Time Complexity: O(n*log(log(n))), Space Complexity: O(n)
+   vll soe(ll n){
+      vector <bool> check(n+1, 1);
+      for(ll i = 2; i*i <= n; i++){
+         for(ll j = i*i; j <= n; j += i){
+            check[j] = 0;
+         }
+      }
+      vll ans;
+      for(ll i = 2; i <= n; i++){
+         if(check[i]) ans.push_back(i);
+      }
+      return ans;
+   }
+
+   // Prime Factorisation of a number with minimal space
+   // Time Complexity: O(sqrt(n)), Space Complexity: O(map.size())
+   map <ll, ll> primeSpace(ll n){
+      map <ll, ll> map;
+      if(!(n&1)){
+         ll count = 0;
+         while(!(n&1)){
+            count++;
+            n /= 2;
+         }
+         map[2] = count;
+      }
+      for(ll j = 3; j*j <= n; j += 2){
+         if(n % j == 0){
+            ll count = 0;
+            while(n % j == 0){
+               count++;
+               n /= j;
+            }
+            map[j] = count;
+         }
+      }
+      if(n > 1) map[n] = 1;
+      return map;
+   }
+
+   // Prime Factorisation of a number with minimal time
+   // Time Complexity: O(log(n)), Space Complexity: O(N)
+   // Call helper function in main.
+   ll N = 1e6;
+   vll sieve(N+1, 0ll);
+   void helper(){
+      for(ll i = 2; i*i <= N; i++){
+         if(sieve[i] == 0){
+            sieve[i] = i;
+            for(ll j = i*i; j <= N; j+= i){
+               if(sieve[j] == 0){
+                  sieve[j] = i;
+               }
+            }
+         }
+      }
+   }
+   map<ll, ll> primeTime(ll n){
+      map <ll, ll> map;
+      ll i = n;
+      while(i != 1){
+         map[sieve[i]]++;
+         i /= sieve[i];
+      }
+      return map;
+   }
+
+
+};
+
+int main(){
+
+   ll N;
+   cin >> N;
+
+   return 0;
+
+}
